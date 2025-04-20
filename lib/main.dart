651f303f73d1cc503eb7c3c3fb0f6d5f71efb911
JokerHin <chokarhin@gmail.com>
2025-04-20 22:41:11 +0800

    stateless stateful setState

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,15 @@ void main() {
   runApp(const MyApp());
 }
 
+String? title = 'Flutter Maps';
+
 // stateless widget
 // material app
 // scaffold
+
+// stateful widget "can refresh the page"
+// stateless widget "can't refresh the page"
+// setState "is used to refresh the page"
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -21,47 +27,65 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.dark,
         ),
       ),
-      home: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Flutter Maps'),
-            centerTitle: true,
-            backgroundColor: Colors.teal,
-            actions: [
-              IconButton(icon: const Icon(Icons.search), onPressed: () {}),
-            ],
-          ),
-          drawer: Drawer(
-            child: Column(
-              children: [
-                DrawerHeader(child: Text('Header')),
-                ListTile(
-                  leading: const Icon(Icons.home),
-                  title: const Text('Home'),
-                ),
-                ListTile(
-                  leading: const Icon(Icons.person),
-                  title: const Text('Person'),
-                ),
-              ],
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title!),
+        centerTitle: true,
+        backgroundColor: Colors.teal,
+        actions: [IconButton(icon: const Icon(Icons.search), onPressed: () {})],
+      ),
+      body:
+          currentIndex == 0
+              ? const Center(child: Text('Home'))
+              : const Center(child: Text('Person')),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            DrawerHeader(child: Text('Header')),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Home'),
             ),
-          ),
-          floatingActionButton: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
-            ],
-          ),
-          bottomNavigationBar: NavigationBar(
-            destinations: [
-              NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-              NavigationDestination(icon: Icon(Icons.person), label: 'Person'),
-            ],
-            onDestinationSelected: (int value) {
-              print(value);
-            },
-          ),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('Person'),
+            ),
+          ],
         ),
+      ),
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
+        ],
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: [
+          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+          NavigationDestination(icon: Icon(Icons.person), label: 'Person'),
+        ],
+        onDestinationSelected: (int value) {
+          setState(() {
+            currentIndex = value;
+          });
+        },
+        selectedIndex: currentIndex,
       ),
     );
   }
