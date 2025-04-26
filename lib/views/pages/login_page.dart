b0d7b79a6084766b92d6d951e87f473bacc666b9
JokerHin <chokarhin@gmail.com>
2025-04-26 16:07:35 +0800

    FractionallySizeBox

--- lib/views/pages/login_page.dart
+++ lib/views/pages/login_page.dart
@@ -41,43 +41,46 @@ class _LoginPageState extends State<LoginPage> {
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Lottie.asset('assets/lotties/Home.json', height: 400.0),
-                TextField(
-                  controller: controllerEmail,
-                  decoration: InputDecoration(
-                    hintText: 'Email',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0),
+            child: FractionallySizedBox(
+              widthFactor: 0.8,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Lottie.asset('assets/lotties/Home.json', height: 400.0),
+                  TextField(
+                    controller: controllerEmail,
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
                     ),
+                    onEditingComplete: () => setState(() {}),
                   ),
-                  onEditingComplete: () => setState(() {}),
-                ),
-                SizedBox(height: 10.0),
-                TextField(
-                  controller: controllerPw,
-                  decoration: InputDecoration(
-                    hintText: 'Password',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0),
+                  SizedBox(height: 10.0),
+                  TextField(
+                    controller: controllerPw,
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
                     ),
+                    onEditingComplete: () => setState(() {}),
                   ),
-                  onEditingComplete: () => setState(() {}),
-                ),
-                SizedBox(height: 20.0),
-                FilledButton(
-                  onPressed: () {
-                    onLoginPressed();
-                  },
-                  style: FilledButton.styleFrom(
-                    minimumSize: Size(double.infinity, 40.0),
+                  SizedBox(height: 20.0),
+                  FilledButton(
+                    onPressed: () {
+                      onLoginPressed();
+                    },
+                    style: FilledButton.styleFrom(
+                      minimumSize: Size(double.infinity, 40.0),
+                    ),
+                    child: Text(widget.title),
                   ),
-                  child: Text(widget.title),
-                ),
-                SizedBox(height: 50.0),
-              ],
+                  SizedBox(height: 50.0),
+                ],
+              ),
             ),
           ),
         ),
