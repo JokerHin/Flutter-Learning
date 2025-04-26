40dbafd9905bd7eba1e7101785dde2d806058e52
JokerHin <chokarhin@gmail.com>
2025-04-26 21:04:30 +0800

    url

--- lib/views/pages/course_page.dart
+++ lib/views/pages/course_page.dart
@@ -1,8 +1,39 @@
 import 'package:first_app/views/widgets/hero_widget.dart';
 import 'package:flutter/material.dart';
+import 'dart:convert' as convert;
 
-class CoursePage extends StatelessWidget {
+import 'package:http/http.dart' as http;
+
+class CoursePage extends StatefulWidget {
   const CoursePage({super.key});
+
+  @override
+  State<CoursePage> createState() => _CoursePageState();
+}
+
+class _CoursePageState extends State<CoursePage> {
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  void getData() async {
+    var url = Uri.https(
+      'dummyjson.com',
+      '/RESOURCE/?limit=10&skip=5&select=key1,key2,key3',
+    );
+
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var jsonResponse =
+          convert.jsonDecode(response.body) as Map<String, dynamic>;
+      var itemCount = jsonResponse['totalItems'];
+      print('Number of books about http: $itemCount.');
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
